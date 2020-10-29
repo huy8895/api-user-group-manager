f411d5794b5e0ddb944bc1bf6d2a9593e18dd5f2
huy8895 <huy8895@gmail.com>
2020-10-29 14:04:06 +0700

    JWT success!

--- src/main/java/com/demoangularspring/angular_spring/repository/IGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demoangularspring.angular_spring.repository;
-
-import com.demoangularspring.angular_spring.model.AppGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IGroupRepository extends JpaRepository<AppGroup,Long> {
-}
--- src/main/java/com/demoangularspring/angular_spring/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demoangularspring.angular_spring.repository;
-
-
-import com.demoangularspring.angular_spring.model.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<AppUser,Long> {
-}
